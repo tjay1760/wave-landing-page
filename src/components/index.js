6a683915113139c2ea76ffc769b41ce6268cbc16
tjay1760 <mcjthiongo@gmail.com>
2024-12-03 14:20:45 +0300

    component setup

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Footer from "./Footer";
+import FAQs from "./FAQs";
+import Navbar from "./Navbar";
+import Hero from "./Hero"
+import FloatingContact from "./FloatingContact";
+import Testimonials from "./Testimonials";
+import GetStarted from "./GetStarted";
+import WhatYouGet from "./WhatYoGet";
+import Tools from "./Tools";
+
+export {FAQs,Footer,FloatingContact,Navbar,Hero,Testimonials,GetStarted,WhatYouGet,Tools}
