cd2f0b2ec9bcff207b40f744b618928173040ac9
tjay1760 <mcjthiongo@gmail.com>
2024-12-04 13:48:59 +0300

    added all navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import './App.css'
-import './index.css'
-import React from 'react'
-import Homepage from './components/Home/Homepage'
-import Navbar from './components/Navigation/Navbar'
-function App() {
+import "./App.css";
+import "./index.css";
+import React from "react";
+import { Route, Routes } from "react-router";
+import Homepage from "./components/Home/Homepage";
+import Navbar from "./components/Navigation/Navbar";
+import Blog from "./components/blog/Blog";
+import Pricing from "./components/pricing/Pricing.jsx";
 
+function App() {
   return (
     <>
-      <div className='w-full'>
-        <Navbar/>
-<Homepage/>
+      <div className="w-full">
+        <Navbar />
       </div>
-        
+      <Routes>
+        <Route path ="/" element = {<Homepage/>}/>
+        <Route path ="/pricing" element = {<Pricing/>}/>
+        <Route path ="/blog" element = {<Blog/>}/>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Navbar = () => {
   return (
     <div className='bg-heroBackgroundColor h-24 w-full'>
       <h1>Tuli</h1>
+      <Link to="/">Home</Link>
+      <Link to ="pricing">Pricing</Link>
+      <Link to ="blog">Blog</Link>
     </div>
   )
 }
