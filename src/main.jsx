306e11280223a5631109e3a60d870b74918c7c34
tjay1760 <mcjthiongo@gmail.com>
2024-12-05 19:48:12 +0300

    fixed linter error

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+import React from 'react'
 import './index.css'
 import App from './App.jsx'
 
@@ -10,5 +11,5 @@ createRoot(document.getElementById('root')).render(
     <App />
     </BrowserRouter>
     
-  </StrictMode>,
+  </StrictMode>
 )
