1d11b5109a197a06fd1539d136379a1a884b3f2f
tjay1760 <mcjthiongo@gmail.com>
2024-12-10 01:08:58 +0300

    background states added

--- src/components/Home/Tools.jsx
+++ src/components/Home/Tools.jsx
@@ -9,19 +9,19 @@ import AdvisoryImg from "../../assets/Dashboard_advisory.png";
 const toolsData = [
   {
     img: InvoicingImg,
-    header:"Invoicing",
+    header: "Invoicing",
     sub: "Stop chasing clients for money",
     desc: "Reduce unpaid invoices, keep tabs on your income, and get paid faster.",
     link: "Learn more about our invoicing feature",
-    route:"/invoicing"
+    route: "/invoicing",
   },
   {
     img: PaymentImg,
-    header:"Payments",
+    header: "Payments",
     sub: "Get paid faster",
     desc: "Convenience is everything. Give your customers the option of paying with one click using a credit card, bank transfer, or Mpesa",
     link: "Learn more about online payments",
-    route: "/payments"
+    route: "/payments",
   },
   {
     img: AccountingImg,
@@ -29,7 +29,7 @@ const toolsData = [
     sub: "Track your income & expenses, painlessly",
     desc: "Monitor your cash flow, stay organized, and stop sweating tax season. Say #sorrynotsorry to your spreadsheets and shoeboxes.",
     link: "Learn more about our accounting feature",
-    route: "/accounting"
+    route: "/accounting",
   },
   {
     img: PayrollImg,
@@ -37,7 +37,7 @@ const toolsData = [
     sub: "Pay your staff, and yourself, in minutes",
     desc: "Didn't go to school for payroll? No worries. We built our payroll tool for small business owners, so it is easy to use AND teaches you as you go.",
     link: "Learn more about our payroll feature",
-    route:"/payroll"
+    route: "/payroll",
   },
   {
     img: AdvisoryImg,
@@ -45,38 +45,57 @@ const toolsData = [
     sub: "Get by with a little 1:1 help",
     desc: "Our (non-judgmental) team of bookkeeping, accounting, and payroll experts is standing by to coach you—or do the work for you.Our (non-judgmental) team of bookkeeping, accounting, and payroll experts is standing by to coach you—or do the work for you.",
     link: "Learn more about wave advisors",
-    route:"/advisory"
+    route: "/advisory",
   },
 ];
 const Tools = () => {
   const [data, setData] = useState(toolsData[0]);
+  const [bgColor, setBgColor] = useState("bg-toolsBackgroundColor2");
   return (
-    <div className="w-full h-80 bg-toolsBackgroundColor1">
-      <div className="h-screen flex justify-center">
+    <div className="w-full min-h-0">
+      <div className={`h-72 flex justify-center  ${bgColor}`}>
         <div className="content-wrapper flex flex-col items-center gap-3">
           <h1 className="text-5xl w-[38rem] text-center text-blue-950 pt-5">
             Our Tools Work Together so You Work Less.
           </h1>
           <div className="buttons mt-5 flex gap-4">
-            <button onClick={()=>setData(toolsData[0])} className="px-5 py-2 text-gray-900 rounded-3xl border-2  border-gray-900 hover:bg-gray-100 hover:border-gray-100">
+            <button
+              onClick={() => {
+                setData(toolsData[0]);
+                setBgColor("bg-toolsBackgroundColor2")
+              }}
+              className="px-5 py-2 text-gray-900 rounded-3xl border-2  border-gray-900 hover:bg-gray-100 hover:border-gray-100 active:bg-gray-100"
+            >
               Invoicing
             </button>
-            <button onClick={()=>setData(toolsData[1])}className="px-5 py-2 text-gray-900 rounded-3xl border-2 border-gray-900 hover:bg-gray-100 hover:border-none">
+            <button
+              onClick={() => {setData(toolsData[1]);                setBgColor("bg-toolsBackgroundColor3")}}
+              className="px-5 py-2 text-gray-900 rounded-3xl border-2 border-gray-900 hover:bg-gray-100 hover:border-none"
+            >
               Payments
             </button>
-            <button onClick={()=>setData(toolsData[2])}className="px-5 py-2 text-gray-900 rounded-3xl border-2 border-gray-900 hover:bg-gray-100 hover:border-none">
+            <button
+              onClick={() => {setData(toolsData[2]);                setBgColor("bg-toolsBackgroundColor4")}}
+              className="px-5 py-2 text-gray-900 rounded-3xl border-2 border-gray-900 hover:bg-gray-100 hover:border-none"
+            >
               Accounting
             </button>
-            <button onClick={()=>setData(toolsData[3])}className="px-5 py-2 text-gray-900 rounded-3xl border-2 border-gray-900 hover:bg-gray-100 hover:border-none">
+            <button
+              onClick={() => {setData(toolsData[3]);                setBgColor("bg-toolsBackgroundColor5")}}
+              className="px-5 py-2 text-gray-900 rounded-3xl border-2 border-gray-900 hover:bg-gray-100 hover:border-none"
+            >
               Payroll
             </button>
-            <button onClick={()=>setData(toolsData[4])}className="px-5 py-2 text-gray-900 rounded-3xl border-2 border-gray-900 hover:bg-gray-100 hover:border-none">
+            <button
+              onClick={() => {setData(toolsData[4]);                setBgColor("bg-toolsBackgroundColor7")}}
+              className="px-5 py-2 text-gray-900 rounded-3xl border-2 border-gray-900 hover:bg-gray-100 hover:border-none"
+            >
               Advisors
             </button>
           </div>
         </div>
       </div>
-      <ToolsScroll data={data}/>
+      <ToolsScroll data={data} color ={bgColor}/>
     </div>
   );
 };
--- src/components/Home/ToolsScroll.jsx
+++ src/components/Home/ToolsScroll.jsx
@@ -1,18 +1,42 @@
 import React from 'react'
 import {Link} from 'react-router'
 import PropTypes from 'prop-types'
+import { Tilt } from 'react-tilt'
+import { FaArrowRight } from "react-icons/fa";
 
-const ToolsScroll = ({data}) => {
+
+
+const defaultOptions = {
+	reverse:        false,  // reverse the tilt direction
+	max:            15,     // max tilt rotation (degrees)
+	perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+	scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
+	speed:          1000,   // Speed of the enter/exit transition
+	transition:     true,   // Set a transition on enter/exit.
+	axis:           null,   // What axis should be disabled. Can be X or Y.
+	reset:          true,    // If the tilt effect has to be reset on exit.
+	easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+}
+
+const ToolsScroll = ({data,color}) => {
+
   return (
-    <div className='w-full h-lvh bg-toolsBackgroundColor2'>
+    <div className={`'w-full h-lvh ${color}`}>
           {console.log(data.header)}
-      <div className="card-holder">
-        <img></img>
-        <div className="card-info">
-          <h1>{data.header}</h1>
-          <h2>{data.sub}</h2>
-          <p>{data.desc}</p>
-          <Link to = {data.route}>{data.link}</Link>
+      <div className="card-holder flex flex-cols justify-center py-11 gap-16 min-h-0">
+        <Tilt options= {defaultOptions}>
+        <div className="glass">
+        <img src={data.img} className='h-96 w-96 object-cover'/>
+        </div>
+        </Tilt>
+        
+        <div className="card-info flex flex-col justify-center gap-2">
+          <h1 className='text-3xl font-bold text-blue-950'>{data.header}</h1>
+          <h2 className='text-2xl font-normal text-blue-950'>{data.sub}</h2>
+          <p className='w-96'>{data.desc}</p>
+          <Link to = {data.route} className='pt-8 text-blue-800 hover:text-blue-500 flex items-center gap-2'>{data.link}
+          <FaArrowRight/>
+          </Link>
         </div>
       </div>
     </div>
@@ -21,8 +45,7 @@ const ToolsScroll = ({data}) => {
 }
 ToolsScroll.propTypes = {
   data:PropTypes.object,
-  header: PropTypes.string,
-  sub: PropTypes.string
-    } 
+  color: PropTypes.string,
+      } 
 
 export default ToolsScroll
