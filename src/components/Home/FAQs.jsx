a16c0360826bb18b72614b24d0ba328020f8db3c
tjay1760 <mcjthiongo@gmail.com>
2024-12-11 08:24:03 +0300

    added stuff

--- src/components/Home/FAQs.jsx
+++ src/components/Home/FAQs.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+const frequentQuestions = [
+{question:"Is tuli Something I need to Install",
+answer: "Tuli tools are web-based, so you just need an Internet connection and browser—no installation is necessary for our online software!"},
+{
+  question:"What if I want to try Tuli to see if it works for my business",
+  answer:"Sign up for the Starter Plan to take the bookkeeping and invoicing basics for a spin. Then, whenever you’re ready, upgrade to the Pro Plan to unlock even more money management features."
+},
+{
+  question:"Is my data secure when I use Tuli?",
+  answer:"Completely. Our servers are protected physically and electronically. Any connections between you, your bank accounts, and Wave are protected by 256-bit SSL encryption. Wave is a PCI-DSS Level 1 Service Provider."
+}
+]
+
 const FAQs = () => {
   return (
-    <div className='w-full h-lvh bg-toolsBackgroundColor1'>FAQs</div>
+    <div className='w-full h-lvh bg-toolsBackgroundColor1'>
+      <h1></h1>
+      <div className="questions">
+        <h1>Is wave So</h1>
+      </div>
+    </div>
   )
 }
 
